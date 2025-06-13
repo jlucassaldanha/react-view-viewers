37c4184ecb7df9132bd95a05deedafd30b641a37
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-06-13 14:27:12 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserSection from './components/UserSection'
+import Counter from './components/Counter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+    <div>
       <header>
         <h2>Espectadores</h2>
       </header>
-  )
+      <Counter count={5}/>
+      <UserSection mods={5}/>
+    </div>
+  ) 
 }
 
 export default App
