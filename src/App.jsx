ba3880ae53bec800ae4ea97b5e05e143d2edce80
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-06-17 12:33:13 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import UserSection from "./components/UserSection";
 import Counter from "./components/Counter";
@@ -7,28 +7,44 @@ import IconUser from "./components/IconUser";
 import Header from "./components/Header";
 
 // Transformar o counter e user section em um só
+const _mods = [
+    {
+      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
+      userName: "ojoojao",
+      id: 2,
+    },
+    {
+      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
+      userName: "ojoojao",
+      id: 2,
+    },
+  ];
 
+  const _users = [
+    {
+      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
+      userName: "ojoojao",
+      id: 2,
+    },
+  ];
 function App() {
-  const mods = [
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-  ];
+  const [mods, setMods] = useState([])
+  const [users, setUsers] = useState([]) 
+  
+  useEffect(() => {
+    const getViewers = async () => {
+      const response = await fetch('API');
+      const data = await response.json();
 
-  const users = [
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-  ];
+      setMods(data.mods);
+      setUsers(data.users);
+    };    
+
+    getViewers();
+    const interval = setInterval(getViewers, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
