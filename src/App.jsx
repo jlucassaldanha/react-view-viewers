812a1f5632f1c3d9ab550518a1d00eef69f8da58
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-06-25 16:56:34 -0300

    update flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from "./Api";
 import "./App.css";
 import UserSection from "./components/UserSection";
 import Counter from "./components/Counter";
@@ -7,27 +7,7 @@ import IconMod from "./components/IconMod";
 import IconUser from "./components/IconUser";
 import Header from "./components/Header";
 
-// Transformar o counter e user section em um só
-const _mods = [
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-  ];
 
-  const _users = [
-    {
-      profileImgURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/c13a4be0-1fa3-4727-a070-efb995966b35-profile_image-70x70.png",
-      userName: "ojoojao",
-      id: 2,
-    },
-  ];
 function App() {
   const [token, setToken] = useState("");
   const [chanel, setChanel] = useState(0);
@@ -41,10 +21,6 @@ function App() {
     setToken(document.location.hash)
   
   }
-
-  const api = axios.create({
-    baseURL: "https://api.twitch.tv/helix",
-  });
 
   api.defaults.headers.common = {
     Authorization: `Bearer ${token.slice(token.indexOf("#access_token=")+14, token.indexOf("&scope"))}`,
@@ -71,20 +47,19 @@ function App() {
 
   return (
     <div>
+      <div>{onGetChanel()}{onGetMod()}{onGetChatters()}</div>
       <Header />
-      {/*<Counter count={users.length + mods.length} />
+      <Counter count={chatters.length} />
       <UserSection 
         icon={<IconMod />} 
         type={"Moderadores"} 
-        users={mods} 
+        users={chatters} 
       />
       <UserSection
         icon={<IconUser fillColor="fill-white" />}
         type={"Usuários"}
-        users={users}
-      />*/}
-      
-      <div>{onGetChanel()}{onGetMod()}{onGetChatters()}</div>
+        users={chatters}
+      />
       
       <div className="text-amber-100">
         {token.slice(token.indexOf("#access_token=")+14, token.indexOf("&scope"))}
--- src/components/UserSection.jsx
+++ src/components/UserSection.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import User from "./User";
+import api from "../Api";
 
 function UserSection({ icon, type, users }) {
+  const [usersInfo, setUsersInfo] = useState()
+
+  function onGetModsInfo() {
+    api.get()
+  }
+
+  function onGetUsersInfo() {
+    api.get()
+  }
+
   return (
     <div>
       <div className="flex gap-2 mt-8 ml-8 mr-8 pl-1.5 py-1.5  bg-[#1F1F23]">
